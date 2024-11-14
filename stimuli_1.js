b25a2ed340c5af1fba5823c777cd474106faa109
SillonComodo <102740763+SillonComodo@users.noreply.github.com>
2024-11-14 11:21:10 -0600

    Add files via upload

---
+++ stimuli_1.js
@@ -0,0 +1,108 @@
+var stimuli_1 = [
+
+{
+sentence: "Juan se relajó jugando una ronda de",
+word: "ajedrez",
+type: "A",
+sim: "0.6",
+fam: "4.746",
+imag: "5.318",
+conc: "5.804"
+},
+
+{
+sentence: "En el jardín había un pequeño",
+word: "árbol",
+type: "A",
+sim: "0.87",
+fam: "6.678",
+imag: "6.292",
+conc: "5.921"
+},
+
+{
+sentence: "En el centro comercial vi a un",
+word: "amigo",
+type: "A",
+sim: "0.44",
+fam: "6.781",
+imag: "6.175",
+conc: "5.853"
+},
+
+{
+sentence: "La constructora contrató a un nuevo",
+word: "arquitecto",
+type: "A",
+sim: "0.4",
+fam: "4.724",
+imag: "4.617",
+conc: "5.959"
+},
+
+{
+sentence: "Cuando caminaba por la calle me encontré un",
+word: "billete",
+type: "A",
+sim: "0.71",
+fam: "6.514",
+imag: "6.514",
+conc: "4.933"
+},
+
+{
+sentence: "En la tienda departamental, vi una",
+word: "blusa",
+type: "A",
+sim: "0.63",
+fam: "5.681",
+imag: "5.195",
+conc: "6.400"
+},
+
+{
+sentence: "Como salió muy apurada, olvidó llevar su",
+word: "bolsa",
+type: "A",
+sim: "0.57",
+fam: "6.416",
+imag: "5.035",
+conc: "4.724"
+},
+
+{
+sentence: "En el cajón de la bodega, encontré una",
+word: "caja",
+type: "A",
+sim: "0.76",
+fam: "6.533",
+imag: "6.232",
+conc: "4.581"
+},
+
+{
+sentence: "El médico le aconsejó mudarse al",
+word: "campo",
+type: "A",
+sim: "0.45",
+fam: "6.334",
+imag: "5.963",
+conc: "4.773"
+},
+
+{
+sentence: "El técnico ajustó las luces del",
+word: "carro",
+type: "A",
+sim: "0.62",
+fam: "5.480",
+imag: "5.966",
+conc: "5.565"
+}
+]
+
+
+
+
+
+
